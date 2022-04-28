e2cc1b02665241b46a9e9a2a5fd2c6245d903776
motoole <motoole76@gmail.com>
2022-04-27 21:39:29 -0400

    Added gameData.json. Deleted unwanted files

--- src/Content.js
+++ src/Content.js
@@ -1,20 +1,25 @@
+import * as React from 'react';
 import './App.css';
+
+import gameData from './data/gameData.json';
+
 import { 
   Typography, 
   Box 
 } from '@mui/material';
 
 
-
 const Content = () => {
+
+  const data = gameData[0]['gamesData'];
+
    return (
     <div className="Content">
       <Box>
-        <Typography>
+        <Typography variant='h3'>
           Content
         </Typography>
-
-        </Box>
+      </Box>
     </div>
   );
 }
