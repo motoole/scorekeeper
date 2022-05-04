e9b060cc4dbe88d5ff5990d5e154cbebbfc2f70a
motoole <motoole76@gmail.com>
2022-05-03 21:14:00 -0400

    First pass at dynamic player textfield adding. Still needs cleanup.

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -12,21 +12,52 @@ import {
   IconButton 
 } from '@mui/material';
 
+import { Close } from '@mui/icons-material';
+
 const CreateGame = () => {
   const current = new Date();
 
   const [gameName, setGameName] = useState("");
-  const [players, setPlayers] = useState("");
+  // const [players, setPlayers] = useState("");
+
+  const [playerNames, setPlayerNames] = useState([{ player: '' }])
+
+  let handleChange = (i, e) => {
+      console.log(playerNames)
+      let newPlayerNames = [...playerNames];
+      newPlayerNames[i][e.target.player] = e.target.value;
+      setPlayerNames(newPlayerNames);
+    }
+  
+  let addFormFields = () => {
+      setPlayerNames([...playerNames, { player: "" }])
+    }
+  
+  let removeFormFields = (i) => {
+      let newPlayerNames = [...playerNames];
+      newPlayerNames.splice(i, 1);
+      setPlayerNames(newPlayerNames)
+  }
+  
+  let handleSubmit = (event) => {
+      event.preventDefault();
+      alert(JSON.stringify(playerNames));
+  }
 
   const handleCreateGame = () =>{
     console.log(gameName);
-    console.log(players.split(','))
+    console.log(JSON.stringify(playerNames));
   }
 
   return(
     <div className='createGame'>
       <Container sx={{ marginTop: '90px' }}>
-        <Typography variant="h5" component="div">New Game</Typography>
+        <Typography variant="h5" component="div">
+          New Game
+          <IconButton aria-label="cancel" color="primary" component={Link} to="/" sx={{ float: 'right' }}>
+            <Close />
+          </IconButton>
+        </Typography>
         <Divider sx={{ margin: '20px 0' }} />
 
         <Typography component="div" variant="div" sx={{ margin: '20px 0' }}>
@@ -40,7 +71,7 @@ const CreateGame = () => {
           />
         </Typography>
         
-        <Typography component="div" variant="div" sx={{ margin: '20px 0' }}>
+        {/* <Typography component="div" variant="div" sx={{ margin: '20px 0' }}>
           <TextField 
             id="createGamePlayers" 
             label="Players" 
@@ -49,11 +80,30 @@ const CreateGame = () => {
             value={players} 
             onChange={(e) => {setPlayers(e.target.value)}}
           />
-        </Typography>
-
+        </Typography> */}
+        {playerNames.map((element, index) => (
+          <div key={index}>
+            <Typography component="div" variant="div" sx={{ margin: '20px 0' }}>
+              <TextField 
+                label={"Player Name"} 
+                variant="filled" 
+                sx={{ width: '100%'}} 
+                name="player"
+                value={ element.playerNames } 
+                onChange={(e) => { handleChange(index, e) }}
+              />
+              {
+                index ? 
+                  <button type="button"  className="button remove" onClick={() => removeFormFields(index)}>Remove</button> 
+                : null
+              }
+            </Typography>
+          </div>
+        // <input type="text" name="player" value={element.player || ""} onChange={e => handleChange(index, e)} />
+        ))}
         <Typography component="div" variant="div" sx={{ margin: '20px 0' }}>
           <Stack spacing={2} direction="row" justifyContent="space-between">
-            <Button variant="outlined" component={Link} to="/">Cancel</Button>
+            <Button variant="outlined" onClick={(e) => addFormFields()}>Add Player</Button>
             <Button variant="contained" onClick={ handleCreateGame }>Create Game</Button>
           </Stack>
         </Typography>
